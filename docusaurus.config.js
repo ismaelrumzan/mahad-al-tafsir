c25ef3b10e201600a77087ab42be02cc8a870121
Macbook2014 <ismael.rumzan@gmail.com>
2022-04-26 23:56:52 +0300

    switch to arabic i8n

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,94 +1,97 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Tafsir Institute',
-  tagline: 'Learn the art of tafsir',
-  url: 'https://tafsir.institute',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'ismaelrumzan', // Usually your GitHub org/user name.
-  projectName: 'mahad-al-tafsir', // Usually your repo name.
+  title: "معهد التفسير",
+  tagline: "التسجيلات الصوتية للشيخ علي هاني العقرباوي",
+  url: "https://tafsir.institute",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "ismaelrumzan", // Usually your GitHub org/user name.
+  projectName: "mahad-al-tafsir", // Usually your repo name.
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          path:'lessons',
-          routeBasePath: 'lessons',
-          sidebarPath: require.resolve('./sidebars.js'),
+          path: "lessons",
+          routeBasePath: "lessons",
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
   plugins: [
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'quran',
-        path: 'quran',
-        routeBasePath: 'quran',
-        sidebarPath: require.resolve('./sidebars.js'),
+        id: "quran",
+        path: "quran",
+        routeBasePath: "quran",
+        sidebarPath: require.resolve("./sidebars.js"),
         // ... other options
       },
     ],
   ],
-
+  i18n: {
+    defaultLocale: "ar",
+    locales: ["ar"],
+  },
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Tafsir Institute',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "معهد التفسير",
+          src: "img/logo.jpeg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'تدريس',
-            position: 'left',
-            label: 'Lessons',
+            type: "doc",
+            docId: "تدريس",
+            position: "left",
+            label: "الدروس التأسيسية",
           },
           {
-            type: 'doc',
-            docsPluginId: 'quran',
-            docId: 'surahs',
-            position: 'left',
-            label: 'Tafsir',
+            type: "doc",
+            docsPluginId: "quran",
+            docId: "surahs",
+            position: "left",
+            label: "تفسير تحليلي",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Lessons',
+            title: "Lessons",
             items: [
               {
-                label: 'Get Started',
-                to: '/lessons/تدريس',
+                label: "Get Started",
+                to: "/lessons/تدريس",
               },
             ],
           },
           {
-            title: 'Tafsir',
+            title: "Tafsir",
             items: [
               {
-                label: 'List of Surahs',
-                to: '/quran/surahs',
+                label: "List of Surahs",
+                to: "/quran/surahs",
               },
             ],
           },
