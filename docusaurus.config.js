f25167d358b1cc33de156c46f550a3c2347e2ca6
Macbook2014 <ismael.rumzan@gmail.com>
2022-04-27 00:07:28 +0300

    broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: "التسجيلات الصوتية للشيخ علي هاني العقرباوي",
   url: "https://tafsir.institute",
   baseUrl: "/",
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
   organizationName: "ismaelrumzan", // Usually your GitHub org/user name.
