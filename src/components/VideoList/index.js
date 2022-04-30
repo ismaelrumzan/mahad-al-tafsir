a6421ee6667c1cde1178c6583a2d3b4950bff497
Ismael Rumzan <ismael.rumzan@gmail.com>
2022-04-30 16:20:56 +0300

    fix build with no snippets

--- src/components/VideoList/index.js
+++ src/components/VideoList/index.js
@@ -12,17 +12,20 @@ export default function VideoList({ children, list = [], data = {} }) {
       setItemIndex(window.location.hash.replace("#", ""));
     }
   }, [vidItem]);
-  data.items.sort((a, b) => {
-    const orderNoArrayA = a.snippet.title.split(" ")[0].split(".");
-    const orderNoArrayB = b.snippet.title.split(" ")[0].split(".");
-    const orderNoA = Number(orderNoArrayA[orderNoArrayA.length - 1]);
-    const orderNoB = Number(orderNoArrayB[orderNoArrayB.length - 1]);
-    if (orderNoA < orderNoB) {
-      return -1;
-    } else {
-      return 1;
-    }
-  });
+  if ("items" in data) {
+    data.items.sort((a, b) => {
+      const orderNoArrayA = a.snippet.title.split(" ")[0].split(".");
+      const orderNoArrayB = b.snippet.title.split(" ")[0].split(".");
+      const orderNoA = Number(orderNoArrayA[orderNoArrayA.length - 1]);
+      const orderNoB = Number(orderNoArrayB[orderNoArrayB.length - 1]);
+      if (orderNoA < orderNoB) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+  }
+
   function changeItem(i) {
     setvidItem(i);
   }
@@ -32,30 +35,34 @@ export default function VideoList({ children, list = [], data = {} }) {
         .replace(/[0-9]/g, "")
         .replace(/\./g, "")
         .replace(/\_/g, "")}`}</h2>
-      {list.length > 0 && (
+      {list.length > 0 && "items" in data && (
         <div className={styles.container}>
           <div className={styles.video}>
             <YouTube youTubeId={list[itemIndex].id} />
           </div>
           <div className={styles.videoNav}>
-            <h3>Next Up</h3>
-            <a
-              onClick={() => changeItem(list[Number(itemIndex) + 1])}
-              className={styles.btnContainer}
-              href={`#${Number(itemIndex) + 1}`}
-            >
-              <img
-                className={styles.backgroundimage}
-                src={
-                  data.items[Number(itemIndex) + 1].snippet.thumbnails.medium
-                    .url
-                }
-              />
-              <div className={styles.overlay}>
-                <img src={useBaseUrl("/img/ic_play.png")} />
-                <p>{data.items[Number(itemIndex) + 1].snippet.title}</p>
-              </div>
-            </a>
+            {Number(itemIndex) < list.length - 1 && (
+              <>
+                <h3>Next Up</h3>
+                <a
+                  onClick={() => changeItem(list[Number(itemIndex) + 1])}
+                  className={styles.btnContainer}
+                  href={`#${Number(itemIndex) + 1}`}
+                >
+                  <img
+                    className={styles.backgroundimage}
+                    src={
+                      data.items[Number(itemIndex) + 1].snippet.thumbnails
+                        .medium.url
+                    }
+                  />
+                  <div className={styles.overlay}>
+                    <img src={useBaseUrl("/img/ic_play.png")} />
+                    <p>{data.items[Number(itemIndex) + 1].snippet.title}</p>
+                  </div>
+                </a>
+              </>
+            )}
 
             <h3>From {list.length} videos</h3>
             <div className="dropdown dropdown--hoverable">
