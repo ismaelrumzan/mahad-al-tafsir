89060c2eae324cfea6190f0374bf2be2afe0f6e4
Ismael Rumzan <ismael.rumzan@gmail.com>
2022-05-01 04:28:36 +0300

    updated meta desc

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      description="التسجيلات الصوتية للشيخ علي هاني العقرباوي"
     >
       <HomepageHeader />
       <main>
