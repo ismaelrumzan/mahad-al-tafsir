75902dd0b395615d2ecde349b7df36f03166e30e
Ismael Rumzan <ismael.rumzan@gmail.com>
2022-05-03 10:54:48 +0300

    video pages mobile

--- src/components/VideoList/index.js
+++ src/components/VideoList/index.js
@@ -42,64 +42,67 @@ export default function VideoList({ children, data = {} }) {
           />
         </div>
         <div className={styles.videoNav}>
-          {Number(itemIndex) < data.items.length - 1 && (
-            <>
-              <p>التسجيل التالى</p>
-              <a
-                onClick={() => changeItem(data.items[Number(itemIndex) + 1])}
-                className={styles.btnContainer}
-                href={`#${Number(itemIndex) + 1}`}
-              >
-                <img
-                  src={
-                    data.items[Number(itemIndex) + 1].snippet.thumbnails.medium
-                      .url
-                  }
-                />
-                <div className={styles.overlay}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="30"
-                    height="30"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="feather feather-play"
-                  >
-                    <polygon points="5 3 19 12 5 21 5 3"></polygon>
-                  </svg>
-                  <p>
-                    {data.items[Number(itemIndex) + 1].snippet.title
-                      .replace(/[0-9]/g, "")
-                      .replace(/\./g, "")
-                      .replace(/\_/g, "")}
-                  </p>
-                </div>
-              </a>
-            </>
-          )}
-
-          <p>من {data.items.length} تسجيلات</p>
-          <div
-            className={clsx("dropdown dropdown--hoverable", styles.selectBtn)}
-          >
-            <button
-              className={clsx("button button--primary", styles.selectBtn)}
+          <div>
+            {Number(itemIndex) < data.items.length - 1 && (
+              <div className={styles.nextItem}>
+                <p>التسجيل التالى</p>
+                <a
+                  onClick={() => changeItem(data.items[Number(itemIndex) + 1])}
+                  className={styles.btnContainer}
+                  href={`#${Number(itemIndex) + 1}`}
+                >
+                  <img
+                    src={
+                      data.items[Number(itemIndex) + 1].snippet.thumbnails
+                        .medium.url
+                    }
+                  />
+                  <div className={styles.overlay}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="30"
+                      height="30"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="feather feather-play"
+                    >
+                      <polygon points="5 3 19 12 5 21 5 3"></polygon>
+                    </svg>
+                    <p>
+                      {data.items[Number(itemIndex) + 1].snippet.title
+                        .replace(/[0-9]/g, "")
+                        .replace(/\./g, "")
+                        .replace(/\_/g, "")}
+                    </p>
+                  </div>
+                </a>
+              </div>
+            )}
+          </div>
+          <div className={styles.listContainer}>
+            <p>من {data.items.length} تسجيلات</p>
+            <div
+              className={clsx("dropdown dropdown--hoverable", styles.selectBtn)}
             >
-              اختر واحدة
-            </button>
-            <ul className="dropdown__menu">
-              {data.items.map((item, index) => (
-                <li onClick={() => changeItem(item)} key={index}>
-                  <a className="dropdown__link" href={`#${index}`}>
-                    {item.snippet.title}
-                  </a>
-                </li>
-              ))}
-            </ul>
+              <button
+                className={clsx("button button--primary", styles.selectBtn)}
+              >
+                اختر واحدة
+              </button>
+              <ul className="dropdown__menu">
+                {data.items.map((item, index) => (
+                  <li onClick={() => changeItem(item)} key={index}>
+                    <a className="dropdown__link" href={`#${index}`}>
+                      {item.snippet.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
