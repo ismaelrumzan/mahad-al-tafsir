dd1705240bc67392e5de5221931126138b8291e0
Ismael Rumzan <ismael.rumzan@gmail.com>
2022-08-13 14:30:19 +0300

    added google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,6 +28,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        googleAnalytics: {
+          trackingID: "G-CRLE7L55TR",
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
